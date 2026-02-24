aa40fe0cd505caab60858a7d35566dd0db78353b
Priyanshu <priyanshupal3110@gmail.com>
2026-02-24 15:44:12 +0530

    test and class work

---
+++ Class Work/script.js
@@ -0,0 +1,91 @@
+// // // console.log("Hello World!");
+// // // var a =10;
+// // // var b =20;
+// // // var c = a + b;
+// // // console.log(c);
+// // //console.log(a);
+// // // console.log(let "introduction");
+// // //let a=10 ;
+// // //let b=30;
+// // //console.log(a);
+// // //date tupe (primitives{7 types} and non-primitives{objects})
+// // //let name = "John"; //string
+// // //let age = 30; //number  
+// // //let isStudent = true; //boolean
+// // //let hobbies = ["reading", "gaming", "coding"]; //array
+
+// // // operators in js (arithmetic operators)
+// // // let a = 10;     
+// // // let b = 20;
+// // // console.log(a + b);
+// // // console.log(a - b);
+// // // console.log(a ** b);
+// // // console.log(a / b);
+// // // console.log(a % b);
+
+
+// // // assignment operators:=, +=, -=, *=, /=, %=
+// // let c = 10;
+// // c += 5;
+// // console.log(c);
+// // c -= 3;
+// // console.log(c); 
+// // c *= 2;
+// // console.log(c);
+// // c /= 4;
+// // console.log(c); 
+// // c %= 3;
+// // console.log(c);
+
+
+// //comparison operators: ==,(loose equality), ===(strict equality), !=, !==, >, <, >=, <=
+// //let x = 10;
+//  let y = "10";
+//     console.log(x == y); // true (loose equality)
+//     console.log(x === y); // false (strict equality)
+//     console.log(x != y); // false (loose inequality)
+
+// conditinol operators: if, else if, else, switch
+// let temperature =25;
+// if(temperature > 30){
+//     console.log("It's a hot day.");
+// }else if(temperature > 20){
+//     console.log("It's a warm day.");
+// }   else if(temperature > 10){
+//     console.log("It's a cool day.");
+// }else{
+//     console.log("It's a cold day.");
+// }   
+// switch case in js
+//  let day = "Monday";
+//     switch(day){        
+//         case 1:
+//             console.log("monday");
+//             break;
+//         case 2:
+//             console.log("tuesday");
+//             break;  
+//         case 3:
+//             console.log("wednesday");
+//             break;
+
+//         case 4:
+//             console.log("thursday");
+//             break;  
+//         case 5:
+//             console.log("friday");
+//             break;  
+//         case 6:
+//             console.log("saturday");
+//             break;
+
+//         case 7:
+//             console.log("sunday");
+//             break;
+
+//         default:
+//             console.log("invalid day");
+//     }
+
+
+
