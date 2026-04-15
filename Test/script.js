9b3f7142270efda539ee2733071b3bdb7ffe847e
Priyanshu <priyanshupal3110@gmail.com>
2026-04-15 23:24:35 +0530

    worked

--- Test/script.js
+++ Test/script.js
@@ -56,36 +56,36 @@
 // }else{
 //     console.log("It's a cold day.");
 // }   
-// switch case in js
-//  let day = "Monday";
-//     switch(day){        
-//         case 1:
-//             console.log("monday");
-//             break;
-//         case 2:
-//             console.log("tuesday");
-//             break;  
-//         case 3:
-//             console.log("wednesday");
-//             break;
+switch case in js
+ let day = "Monday";
+    switch(day){        
+        case 1:
+            console.log("monday");
+            break;
+        case 2:
+            console.log("tuesday");
+            break;  
+        case 3:
+            console.log("wednesday");
+            break;
 
-//         case 4:
-//             console.log("thursday");
-//             break;  
-//         case 5:
-//             console.log("friday");
-//             break;  
-//         case 6:
-//             console.log("saturday");
-//             break;
+        case 4:
+            console.log("thursday");
+            break;  
+        case 5:
+            console.log("friday");
+            break;  
+        case 6:
+            console.log("saturday");
+            break;
 
-//         case 7:
-//             console.log("sunday");
-//             break;
+        case 7:
+            console.log("sunday");
+            break;
 
-//         default:
-//             console.log("invalid day");
-//     }
+        default:
+            console.log("invalid day");
+    }
 
 
 
