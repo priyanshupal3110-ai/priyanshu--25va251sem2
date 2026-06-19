18a0382e867741149fec555baeaf07a1100054b3
Priyanshu <priyanshupal3110@gmail.com>
2026-06-19 15:16:22 +0530

    work

---
+++ my-app/src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <div>
+      <h1>YOUTUBE VIDEO EMBEDDED IN REACT APP
+
+      </h1>
+
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/VzALAIICWI4?si=jLWSb6ANm_-r0PDJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
